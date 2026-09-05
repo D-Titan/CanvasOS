2b93cb59e618037935c6ae83f435385518452ffe
D-Titan <111182645+D-Titan@users.noreply.github.com>
2026-09-05 12:37:24 +0530

    Update registry.js

--- registry.js
+++ registry.js
@@ -57,6 +57,12 @@ window.AppRegistry = [
         hidden: true, // Prevents this from showing up on the dock!
         iconSvg: `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"/><polygon points="10 8 16 12 10 16 10 8"/></svg>`,
         scriptPath: 'apps/MediaEditor.js'
+    },
+    {
+        id: 'DocExporter',
+        name: 'Doc Exporter',
+        iconSvg: `<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"/><polyline points="14 2 14 8 20 8"/><path d="M12 18v-6"/><path d="M9.5 14.5 12 12l2.5 2.5"/></svg>`,
+        scriptPath: 'apps/DocExporter.js'
     }
 
 ];
